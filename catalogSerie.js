6100096828304a147e1185157485013cac45db01
Luca Valenti Jr <lca.valenti@mongibello.tech>
2026-09-23 15:51:27 +0200

    4th try error upload photo from mobile;

--- catalogSerie.js
+++ catalogSerie.js
@@ -431,7 +431,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (formData.immagineFile) {
           const cloudData = new FormData();
           cloudData.append('file', formData.immagineFile);
-          cloudData.append('upload_preset', 'Catalogo');
+          cloudData.append('upload_preset', 'catalogo_test');
           const cloudRes = await fetch('https://api.cloudinary.com/v1_1/dq1io8iet/image/upload', {
             method: 'POST',
             body: cloudData,
